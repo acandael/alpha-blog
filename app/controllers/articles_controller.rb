43d782df8f2d4dffb553698f5d085d7350f55a84
Anthony Candaele <anthony.candaele@tothepoint.company>
2021-07-24 11:49:29 +0200

    implement delete and refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    
   end
 
   def new
@@ -13,7 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def create
@@ -27,7 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+    
     if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
@@ -36,7 +38,17 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
